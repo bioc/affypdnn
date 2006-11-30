146121f46cb269d135dfe8ec909426d94f0abb81
Laurent <laurent@cbs.dtu.dk>
2006-11-30 14:46:55 +0000

    Fixed missing cast of a factor into a string (on which assign was choking)
    git-svn-id: file:///home/git/hedgehog.fhcrc.org/bioconductor/trunk/madman/Rpacks/affypdnn@21317 bc3139a8-67e5-0310-9ffc-ced21a209358

--- R/pdnn.params.chiptype.R
+++ R/pdnn.params.chiptype.R
@@ -93,7 +93,7 @@ pdnn.params.chiptype <- function(energy.param.file, probes.file = NULL, probes.p
                           params.gene = new.env(hash=TRUE))
   
   
-  Eg <- multiassign(as.list(ep[[1]][1:16]), as.list(as.vector(ep[1:16, 2])), envir=params.chiptype$Eg) ## energy (specific)
+  Eg <- multiassign(as.list(as.character(ep[[1]][1:16])), as.list(as.vector(ep[1:16, 2])), envir=params.chiptype$Eg) ## energy (specific)
   
   ##Wg <- multiassign(rownames(ep), as.vector(ep[33:56, 2]), envir=env.list$Wg) ## weights (specific)
   
